25f2d12877933258d90799d4d90d0fc7a67aa02d
wfeng007 <wfeng007@163.com>
2016-03-02 00:17:43 +0800

    封装 tradeorderwaiter

--- src/summ/btc/btclock/SimpleClockDecider.java
+++ src/summ/btc/btclock/SimpleClockDecider.java
@@ -4,20 +4,20 @@
 package summ.btc.btclock;
 
 import java.math.BigDecimal;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-
-import javax.swing.JOptionPane;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import summ.btc.btclock.Kanban.Depth;
-import summ.btc.btclock.btcc.BtccTrader;
-import summ.btc.btclock.btcc.BtccTrader.TradeOption;
 import summ.btc.btclock.data.TradeOrder;
 import summ.btc.btclock.data.TradeOrderStatusEnum;
-import summ.btc.btclock.okcoin.OkcoinMarketProbe;
-import summ.btc.btclock.okcoin.OkcoinTrader;
 
 /**
  * 决策执行者，集中使用算法通知Trader进行买卖。
@@ -26,19 +26,21 @@ import summ.btc.btclock.okcoin.OkcoinTrader;
  */
 public class SimpleClockDecider {
 
-    private Thread            simpleClockTradeWorker = null;
-    private SimpleClockRunner simpleClockRunner      = null;
+	private Thread simpleClockTradeWorker = null;
+	private SimpleClockRunner simpleClockRunner = null;
+
+	private ExecutorService threadpool = null;
     
     private Tradable  trader;
     private Kanban kanban;
 
-    static final String       AMOUNT                 = "0.1000";
+    static final String       AMOUNT                 = "0.0100";
 
     SimpleClockDecider() {
     }
     
     void startup() {
-    	
+    	threadpool=Executors.newFixedThreadPool(10);
     	if(this.simpleClockRunner==null||this.simpleClockRunner.isFinished){
     		this.simpleClockRunner = new SimpleClockRunner(20);
 	    	this.simpleClockTradeWorker = new Thread(simpleClockRunner);
@@ -48,13 +50,179 @@ public class SimpleClockDecider {
     }
 
     void stop() {
+    	threadpool.shutdownNow();
         this.simpleClockRunner.setFinished(true);
     }
     
+
+    public static class TradeOrderWaiter {
+    	
+    	private TradeOrder waitingTradeOrder;
+    	private Tradable trader;
+    	private ExecutorService threadpool;
+    	private Kanban kanban;
+    	
+    	private TradeOrderWaiter(TradeOrder wto,Tradable trader,Kanban kanban,ExecutorService threadpool){
+    		this.waitingTradeOrder =wto;
+    		this.trader=trader;
+    		this.kanban=kanban;
+    		this.threadpool=threadpool;
+    	}
+    	
+    	//不等待
+//    	public TradeOrder canellAndEscpe(){
+//    		TradeOrder to=canell(1000);
+//    		waitingTradeOrder=this.trader.get(waitingTradeOrder.getId());
+//    		//escpe
+//    		return escpe(waitingTradeOrder.getOrigAmount(),waitingTradeOrder.getTradeType());
+//    	}
+//    	private  TradeOrder escpe(String amount,TradeTypeEnum type){
+//    		Depth d = kanban.nowDepth;
+//    		if(TradeTypeEnum.ASK.equals(type)){
+//    			if (d.bidList.size() <= 0) {
+//					throw new RuntimeException("无参考价格用于下单。");
+//				}
+//				TradeOrder to = d.bidList.get(0);
+//	    		return trader.sell(to.getSubmitPrice(), amount);
+//    		}else if(TradeTypeEnum.BID.equals(type)){
+//    			if (d.askList.size() <= 0) {
+//					throw new RuntimeException("无参考价格用于下单。");
+//				}
+//				TradeOrder to = d.askList.get(0);
+//	    		return trader.buy(to.getSubmitPrice(), amount);
+//    		}
+//    		return null;
+//    	}
+
+    	/**
+    	 * 取消该订单
+    	 * @param timeout
+    	 * @return
+    	 */
+    	public TradeOrder canell(long timeout) {
+    		this.trader.cancel(waitingTradeOrder.getId());
+        	Future<TradeOrder> ft=this.threadpool.submit(new WaitingOrderCaller(this.kanban,this.trader,waitingTradeOrder.getId()));
+        	TradeOrder reTo=null;
+        	try {
+        		reTo=ft.get(timeout,TimeUnit.MILLISECONDS);
+			} catch (TimeoutException toe) {
+        		System.out.println("等待订单撤销时超时。"); //超时不用打印
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			if(reTo!=null && ft.isDone()){
+				return reTo;
+			}
+			ft.cancel(true);//不在检测
+        	return this.trader.get(waitingTradeOrder.getId());
+		}
+    	
+    	/**
+    	 * 等待订单成交或取消
+    	 * @return
+    	 */
+		public TradeOrder await() {
+			return this.await(Long.MAX_VALUE);
+		}
+		
+    	/**
+    	 * 等待订单成交或取消，并有超时。
+    	 * 超时返回现有订单情况
+    	 * @return
+    	 */
+		public TradeOrder await(long timeout) {
+        	Future<TradeOrder> ft=this.threadpool.submit(new WaitingOrderCaller(this.kanban,this.trader,waitingTradeOrder.getId()));
+        	TradeOrder reTo=null;
+        	try {
+        		reTo=ft.get(timeout,TimeUnit.MILLISECONDS);
+        	} catch (TimeoutException toe) {
+        		System.out.println("等待订单交易超时。"+toe.getMessage()); //超时不用打印
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			if(reTo!=null && ft.isDone()){
+				return reTo;
+			}
+			ft.cancel(true);//不在检测
+        	return this.trader.get(waitingTradeOrder.getId()); //
+//			return null;
+		}
+    	
+    }
+    
+    /**
+     * 下订单，并返回一个可以担待订单结束的句柄
+     */
+    public TradeOrderWaiter buyWaiter(String price,String amount){
+    	TradeOrder order=offerBuyOrder(price,amount);
+    	return new TradeOrderWaiter(order,this.trader,this.kanban,this.threadpool);
+    }
+    /**
+     * 下订单，并返回一个可以担待订单结束的句柄
+     */
+    public TradeOrderWaiter sellWaiter(String price,String amount){
+    	TradeOrder order=offerSellOrder(price,amount);
+    	return new TradeOrderWaiter(order,this.trader,this.kanban,this.threadpool);
+    }
+    
+	/**
+	 * 
+	 * @author wfeng007
+	 * 
+	 */
+	public static class WaitingOrderCaller implements Callable<TradeOrder>{
+
+		private Thread doingThread;
+		private Kanban kanban;
+		private Tradable trader;
+		private Long id;
+		public WaitingOrderCaller(Kanban kanban,Tradable trader,Long id){
+			this.kanban=kanban;
+			this.trader=trader;
+			this.id=id;
+		}
+		
+		/**
+		 * 考虑可以让外部系统打断正在执行本Callable的机制
+		 */
+		public void interupt(){
+			if(this.doingThread!=null)
+				this.doingThread.interrupt();
+		}
+		
+		/**
+		 * 
+		 */
+		@Override
+		public TradeOrder call() throws Exception {
+			this.doingThread=Thread.currentThread();
+			for (;;) {
+				if(kanban.closedOrderMap.containsKey(id)){
+					return kanban.closedOrderMap.get(id);
+				}else{
+					try {
+						Thread.sleep(200);
+					} catch (InterruptedException e) {
+//						e.printStackTrace();
+//						break;
+						return null;
+					}
+				}
+			}
+			//如果被打断则使用trader再访问一次啊
+//			TradeOrder to=this.trader.get(id);
+//			return to;
+		}
+		
+	}
+	
     /*
      * 下买单 ，买一价。不等待成交。
      */
-    public TradeOrder offerBuyOrder(String amount) {
+    public TradeOrder offerBuyOrder(String price,String amount) {
+			if(price!=null){
+				return trader.buy(price, amount);
+			}
     		Depth d = kanban.nowDepth;
 			if (d.bidList.size() <= 0) {
 				throw new RuntimeException("无参考价格用于下单。");
@@ -68,7 +236,10 @@ public class SimpleClockDecider {
     /*
      * 下卖单，卖一价。不等待成交。
      */
-    public TradeOrder offerSellOrder(String amount) {
+    public TradeOrder offerSellOrder(String price,String amount) {
+    		if(price!=null){
+    			return trader.sell(price, amount);
+    		}
 			Depth d = kanban.nowDepth;
 			if (d.askList.size() <= 0) {
 				throw new RuntimeException("无参考价格用于下单。");
@@ -79,32 +250,26 @@ public class SimpleClockDecider {
 			return trader.sell(to.getSubmitPrice(), amount);
 	}
     
-    public TradeOrder waitingTrade(Long id){
-    		if(id==null){
-    			throw new NullPointerException("id not be Null!");
-    		}
-//    		TradeOrder to=new TradeOrder();
-//    		to.setId(id);
-    		outFor:for(;;){
-    			for (int i = 0; i < 20; i++) {
-    				if(kanban.closedOrderMap.containsKey(id)){
-    					return kanban.closedOrderMap.get(id);
-    				}else{
-    					try {
-							Thread.sleep(500);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-    				}
-				}
-    			//通过远端访问
-    			TradeOrder to=this.trader.get(id);
-    			System.out.println(to);
-    			if(TradeOrderStatusEnum.CLOSED.equals(to.getStatus())){
-    				return to;
-    			}
-    		}
-    }
+//    /**
+//     * 
+//     * @param amount
+//     * @return
+//     */
+//    public Future<TradeOrder> offerSellOrderFuture(String amount) {
+//    	TradeOrder order=offerSellOrder(amount);
+//    	return threadpool.submit(new WaitingOrderCaller(this.kanban,this.trader,order.getId()));
+//    }
+//    /**
+//     * 
+//     * @param amount
+//     * @return
+//     */
+//    public Future<TradeOrder> offerBuyOrderFuture(String amount) {
+//    	TradeOrder order=offerBuyOrder(amount);
+//    	FutureTask<TradeOrder> ft=new FutureTask<TradeOrder>(new WaitingOrderCaller(this.kanban,this.trader,order.getId()));
+//    	threadpool.submit(ft,order);
+//    	return ft;
+//    }
     
     public static class Innings {
     	public TradeOrder entry;
@@ -172,15 +337,26 @@ public class SimpleClockDecider {
 
         //一局（进出组成的一组交易，最简单的是进出各1单）
         /**
+         * @throws TimeoutException 
+         * @throws ExecutionException 
+         * @throws InterruptedException 
          * @TODO 继续
          */
         @SuppressWarnings("unused")
         private Innings biz() {
         	Innings inni=new Innings();
             //1下买入市价单1单，需要注意处理长时间部分成交。
-            TradeOrder toB = offerBuyOrder(AMOUNT);
-            toB=waitingTrade(toB.getId()); //TODO 增加超时撤单，并判断是否为entry则重试。
-            inni.entry=toB;
+        	TradeOrder toB=null;
+        	for(;;){ //一直等到买到为止
+	        	TradeOrderWaiter tow=buyWaiter(null,AMOUNT);
+	        	toB=tow.await(5000);
+	        	if(toB==null || !TradeOrderStatusEnum.CLOSED.equals(toB.getStatus())){
+	        		toB=tow.canell(1000);
+	        	}else{
+	        		inni.entry=toB;
+	        		break;
+	        	}
+        	}
             
             //3针对成交价计算卖出价
             String strikePriceStr = null;
@@ -193,27 +369,22 @@ public class SimpleClockDecider {
             System.out.println("建仓价：" + strikePriceStr + "  准备平仓价：" + newPriceStr);
 
             //4下卖出价
-            TradeOrder toS = trader.sell(newPriceStr, AMOUNT);
+            TradeOrder toS = null;
             //检测是否在超时周期内交易成功
-            toS=waitingTrade(toS.getId());//TODO 增加超时撤单，并判断是否为exit则市价强制卖单。
-            inni.exit=toS;
-
+            TradeOrderWaiter tow=sellWaiter(newPriceStr,AMOUNT);
+            for(;;){
+	            toS=tow.await(5000);//如果5000中无法交易成功立即重新下卖单 并使用sell1价格
+	            if(toS==null || !TradeOrderStatusEnum.CLOSED.equals(toS.getStatus())){
+	            	toS=tow.canell(1000);
+	            }else{
+	            	inni.exit=toS;
+	        		break;
+	            }
+	            System.out.println("市价单卖");
+	            tow=sellWaiter(null,AMOUNT);
+            }
+            
             //完成处理
-/*            if (TradeOrderStatusEnum.CLOSED.equals(toS.getStatus())) {
-                System.out.println("一局交易成功！");
-                System.out.println(toS);
-            } else {//5超时未出单取消订单并直接市价出单
-                System.out.println("一局交易超时未出单，取消订单并直接市价出单！");
-
-                //超时，取消之前订单
-                trader.cancel(Long.valueOf(orderId2));
-
-                //超时，市价单平仓
-                TradeOption sell2Opt = new TradeOption();
-                sell2Opt.setWaitingClose(false);
-//                trader.sell(null, AMOUNT, sell2Opt);
-
-            }*/
             inni.parseProfit();
             System.out.println(inni.profit.toPlainString());
             return inni;
